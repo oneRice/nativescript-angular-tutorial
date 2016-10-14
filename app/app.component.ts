8992d7f06de56fb01891c51b36457cae1e8854d7
oneRice <onericeyifan@gmail.com>
2016-10-14 15:09:54 +0200

    Building the UI

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from "@angular/core";
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
+    styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {
     

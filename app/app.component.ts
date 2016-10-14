4a829b26f5cf6208d98e6c7842dcd32dc5aa58dc
oneRice <onericeyifan@gmail.com>
2016-10-14 16:39:51 +0200

    Application Logic-Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,50 @@
 import {Component} from "@angular/core";
 
+import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
+
 @Component({
     selector: "my-app",
-    templateUrl: "app.component.html",
-    styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+    templateUrl: "pages/login/login.component.html",
+    styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
+    providers: [UserService]
 })
 export class AppComponent {
-    
+    user: User;
+    isLogged = true;
+
+    constructor(private userService: UserService) {
+        this.user = new User();
+        this.user.email = "user@nativescript.org";
+        this.user.password = "password";
+    }
+
+    submit() {
+        if (this.isLogged) {
+            this.login()
+        } else {
+            this.signup()
+        }
+    }
+
+    login() {
+        // TODO
+    }
+
+    signup() {
+        this.userService.register(this.user)
+            .subscribe(
+                () => {
+                    alert("Your account was successfully created.");
+                    this.toggleDisplay();
+                },
+                () => {
+                    alert("Unfortunately, we were unable to create your account.");
+                }
+            );
+    }
+
+    toggleDisplay() {
+        this.isLogged = !this.isLogged;
+    }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [NativeScriptModule],
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        NativeScriptHttpModule
+    ],
     bootstrap: [AppComponent]
 })
 
